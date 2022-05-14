ceae839db56225ca54dc88cddf53dbdbd76d692d
U180709061 <hakkicanbuluc@gmail.com>
2022-05-14 19:05:49 +0300

    updated Box contract

--- contracts/Box.sol
+++ contracts/Box.sol
@@ -3,17 +3,19 @@
 
 pragma solidity ^0.8.0;
 
-contract Box {
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Box is Ownable {
     uint256 private _value;
 
     event ValueChanged(uint256 value);
 
-    function store(uint256 value) public {
+    function store(uint256 value) public onlyOwner {
         _value = value;
         emit ValueChanged(value);
     }
 
-    function retrieve() public view returns (uint256 value) {
+    function retrieve() public view returns (uint256) {
         return _value;
     }
 }
