9bf145860c9a50b7954a2d3339e6747eb574f0e4
U180709061 <hakkicanbuluc@gmail.com>
2022-06-18 18:00:11 +0300

    token deployed

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,15 @@
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+require("@nomiclabs/hardhat-ethers");
+require("dotenv").config();
+
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: process.env.infuraAPI,
+      accounts: { mnemonic: process.env.mnemonic },
+    },
+  },
 };
